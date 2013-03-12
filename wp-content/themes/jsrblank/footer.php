5dd23d8aa2fe591d2dc336335fa09aeff3b6d213
Jake Ruesch <jsruesch@gmail.com>
2013-03-12 11:50:47 -0500

    Initial commit

---
+++ wp-content/themes/jsrblank/footer.php
@@ -0,0 +1,39 @@
+<?php wp_footer(); ?>
+
+<div class="footer">
+    
+    <div class="host_and_sponsor_container">
+        
+        <div class="hosts">
+            <div class="hosts_header"></div>
+            <div class="hosts_logo">
+                <a href="" target="_blank"> <div class="hosts_logo_wid"></div> </a>
+                <a href="" target="_blank"> <div class="hosts_logo_lgn"></div> </a>
+            </div>
+        </div>
+        
+        <div class="sponsors">
+            <div class="sponsors_header"></div>
+            <div class="sponsors_logo">
+                <a href="" target="_blank"> <div class="sponsors_logo_gates"></div> </a>
+                <a href="" target="_blank"> <div class="sponsors_logo_drc"></div> </a>
+                <a href="" target="_blank"> <div class="sponsors_logo_sf"></div> </a>
+            </div>
+        </div>
+        
+    </div>
+    
+    <div class="old_sites_and_contact_container">
+        <a href="" class="floatleft">Click here to view proceedings, websites, and schedules from previous years</a>
+        <p class="floatright">Contact GLS at : <span class="lighter_blue">( 608 ) 265-8739 </span> </p>
+    </div>
+</div>
+    
+    
+    
+<div class="clear">
+</div>	
+	
+</body>
+
+</html>
---
+++ wp-content/themes/jsrblank/home_page.php
@@ -0,0 +1,234 @@
+<?php
+/*
+ Template Name: Home Page Template
+*/
+?>
+
+
+<?php get_header(); ?>
+
+
+<!-- full width slider -->
+<div id="fwslider">
+        <div class="slider_container">
+            
+            <div class="slide"> 
+                <!-- Slide image -->
+                <img src="wp-content/themes/jsrblank/images/slider_images/gls_slider_banner4.png">
+                <!-- /Slide image -->
+                        
+                <!-- Texts container -->
+                <div class="slide_content">
+                    <div class="slide_content_wrap">
+                    </div>
+                </div>
+                <!-- /Texts container -->
+            </div>
+
+            <div class="slide"> 
+                <img src="wp-content/themes/jsrblank/images/slider_images/people.png">
+                <div class="slide_content">
+                    <div class="slide_content_wrap">
+                        <h4 class="title">PEOPLE DOING THINGS</h4>
+                        <p class="description">Look at all them there people.</p>
+                        <a class="readmore" href="http://www.google.com">Learn More</a>
+                        <!-- /Learn more button -->
+                    </div>
+                </div>
+            </div>
+                    
+            <div class="slide"> 
+                <img src="wp-content/themes/jsrblank/images/slider_images/sculpt.png">
+                <div class="slide_content">
+                    <div class="slide_content_wrap">
+                        <h4 class="title">3D SCULPT</h4>
+                        <p class="description">See this and more at the art exhibition</p>
+                        <a class="readmore" href="http://www.google.com">Learn More</a>
+                        <!-- /Learn more button -->
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+        <div class="timers"></div>
+        <div class="slidePrev"><span></span></div>
+        <div class="slideNext"><span></span></div>
+    </div>
+
+
+
+
+<!-- page wrap -->
+
+<div class="page_wrap">
+    
+    <div class="features">
+                
+        <!-- Location Feature -->
+        <div class="feature_container floatleft">
+            
+            <div class="feature_header_container">
+                
+                <div class="feature_map_icon"></div>
+                
+                <div class="feature_header_content">
+                    <p class="feature_title">Location</p>
+                    <p class="feature_subtitle">Memorial Union, Madison, WI</p>
+                </div>
+                
+            </div>
+            
+            <div class="feature_content">
+                <a href=""> <div class="static_map"></div> </a>
+            </div>
+            
+        </div>
+        
+        
+        <!-- Dates Feature -->
+        <div class="feature_container floatleft">
+            
+            <div class="feature_header_container">
+                
+                <div class="feature_calendar_icon"></div>
+                
+                <div class="feature_header_content">
+                    <div class="floatleft">
+                        <p class="feature_title">Date</p>
+                        <p class="feature_subtitle">GLSC 9.0!</p>
+                    </div>
+                    
+                    <div class="register_button_container">
+                        <a href=""> <div class="register_button"></div> </a>
+                    </div>
+                </div>
+                
+            </div>
+            
+            <div class="feature_content">
+                
+                <a href="">
+                    <div class="feature_date_container">
+                        <p class="feature_date_pink">Pre-Conference Activities</p>
+                        <p class="feature_date_white">June 11, 2013</p>
+                    </div>
+                </a>
+                
+                <a href="">                
+                    <div class="feature_date_container">
+                        <p class="feature_date_pink">GLS Conference</p>
+                        <p class="feature_date_white">June 12-14, 2013</p>
+                    </div>
+                </a>
+                
+            </div>
+            
+        </div>
+        
+        
+        <!-- Schedule Feature -->
+        <div class="feature_container floatright">
+            
+            <div class="feature_header_container">
+                
+                <div class="feature_clipboard_icon"></div>
+                
+                <div class="feature_header_content">
+                    <p class="feature_title">Schedule</p>
+                    <p class="feature_subtitle">All Kinds of Goodies</p>
+                </div>
+                
+            </div>
+            
+            <div class="feature_content">
+                <a href=""> <div class="static_map"></div> </a>
+            </div>
+            
+        </div>
+        
+    </div>
+    
+    
+    
+</div> <!-- end page wrap -->
+
+<div class="keynote_container">
+    
+    <div class="keynotes">
+        <div class="keynote_header"></div>
+        
+        <div class="speakers_container">
+            
+            
+            <!-- Dani Herro -->
+            <a href="">
+                <div class="speaker">
+                    
+                    <div class="speaker_dani"></div>
+                    
+                    <div class="speaker_label">
+                        <p class="speaker_name">Dani Herro</p>
+                        <p class="speaking_at">Playful Learning Keynote Speaker</p>
+                        
+                    </div>
+                    
+                </div>
+            </a>
+            
+            
+            <!-- Dani Mary -->
+            <a href="">
+                <div class="speaker">
+                    
+                    <div class="speaker_mary"></div>
+                    
+                    <div class="speaker_label">
+                        <p class="speaker_name">Mary Flanagan</p>
+                        <p class="speaking_at">GLS Keynote Speaker</p>
+                        
+                    </div>
+                    
+                </div>
+            </a>
+            
+            
+            <!-- Dani Steve -->
+            <a href="">
+                <div class="speaker">
+                    
+                    <div class="speaker_steve"></div>
+                    
+                    <div class="speaker_label">
+                        <p class="speaker_name">Steve Schoettler</p>
+                        <p class="speaking_at">GLS Keynote Speaker</p>
+                        
+                    </div>
+                    
+                </div>
+            </a>
+            
+            
+            <!-- Dani TBD -->
+            <a href="">
+                <div class="speaker">
+                    
+                    <div class="speaker_dani"></div>
+                    
+                    <div class="speaker_label">
+                        <p class="speaker_name">TBD</p>
+                        <p class="speaking_at">GLS Keynote Speaker</p>
+                        
+                    </div>
+                    
+                </div>
+            </a>
+            
+        </div>
+        
+    </div>
+    
+</div>
+
+
+<?php get_footer(); ?>
