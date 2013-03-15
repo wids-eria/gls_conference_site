5228fef0775986afce4e054a69fbc3757f010607
Jake Ruesch <jsruesch@gmail.com>
2013-03-15 11:29:21 -0500

    Add social media and old site links to footer

--- wp-content/themes/jsrblank/footer.php
+++ wp-content/themes/jsrblank/footer.php
@@ -7,26 +7,39 @@
         <div class="hosts">
             <div class="hosts_header"></div>
             <div class="hosts_logo">
-                <a href="" target="_blank"> <div class="hosts_logo_wid"></div> </a>
-                <a href="" target="_blank"> <div class="hosts_logo_lgn"></div> </a>
+                <a href="http://wid.wisc.edu/research/" target="_blank"> <div class="hosts_logo_wid"></div> </a>
+                <a href="http://www.learninggamesnetwork.org/" target="_blank"> <div class="hosts_logo_lgn"></div> </a>
             </div>
         </div>
         
         <div class="sponsors">
             <div class="sponsors_header"></div>
             <div class="sponsors_logo">
-                <a href="" target="_blank"> <div class="sponsors_logo_gates"></div> </a>
-                <a href="" target="_blank"> <div class="sponsors_logo_drc"></div> </a>
-                <a href="" target="_blank"> <div class="sponsors_logo_sf"></div> </a>
+                <a href="http://www.gatesfoundation.org/" target="_blank"> <div class="sponsors_logo_gates"></div> </a>
+                <a href="http://www.datarecognitioncorp.com/Pages/default.aspx" target="_blank"> <div class="sponsors_logo_drc"></div> </a>
+                <a href="http://www.sonicfoundry.com/" target="_blank"> <div class="sponsors_logo_sf"></div> </a>
             </div>
         </div>
         
     </div>
     
+    
     <div class="old_sites_and_contact_container">
-        <a href="" class="floatleft">Click here to view proceedings, websites, and schedules from previous years</a>
-        <p class="floatright">Contact GLS at : <span class="lighter_blue">( 608 ) 265-8739 </span> </p>
+        
+	<div class="old_sites_container floatleft">
+		<a href="http://localhost/glsc_wp/?page_id=148">Click here to view proceedings, websites, and schedules from previous years</a>
+	</div>
+        
+	<div class="social">
+		<a href="https://secure.flickr.com/photos/gls-conference" target="_blank"> <div class="flickr_button floatright"></div> </a>
+		<a href="https://twitter.com/GLScenter" target="_blank"> <div class="twitter_button floatright"></div> </a>
+		<a href="https://www.facebook.com/glsconference" target="_blank"> <div class="facebook_button floatright"></div> </a>
+	</div>
+	
+	<p class="floatright">Contact GLS at : <span class="lighter_blue">( 608 ) 265-8739 </span> </p>
+
     </div>
+    
 </div>
     
     
